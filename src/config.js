947293eb2dd91dcd5d3b337c6a0f83e11ba01e5d
lev <wot.k@mail.ru>
2022-09-05 18:34:02 +0300

    fix addrs again

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-let backend_container = 'posvyat-backend:3001'
-let bot_container =  'cstatibot:8000'
+let backend_container = 'posvyat-backend:3000'
+let bot_container =  'cstatibot:8001'
 
 module.exports = {
     backend_container,
